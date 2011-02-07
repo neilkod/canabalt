3792690bd792600f9465b3b05431cdb2268b8211
neilkod <nkodner@gmail.com>
2011-02-07 17:45:42 -0500

    added grouping by device and by death type

--- scores_by_death.r
+++ scores_by_death.r
@@ -3,10 +3,13 @@ library('ggplot2')
 canabalt <- read.csv('/Users/nkodner/Dropbox/development/R/canabalt/data/canabaltdetailedscores.csv',stringsAsFactors=FALSE)
 names(canabalt)<-c('score','death','device')
 
-# group on type of death and compute the count, mean, stddev, median, range.
+# group on type of death and the device and compute the count, mean, stddev, median, range.
 # we'll use this later
-grpd<-ddply(canabalt,"death",function(df)c(length(df$score),round(mean(df$score)),round(sd(df$score)),median(df$score),max(df$score),min(df$score),max(df$score)-min(df$score)))
-names(grpd) <- c('death','count','mean','stddev','median','max','min','range')
+bydeathtype<-ddply(canabalt,"death",function(df)c(length(df$score),round(mean(df$score)),round(sd(df$score)),median(df$score),max(df$score),min(df$score),max(df$score)-min(df$score)))
+names(bydeathtype) <- c('death','count','mean','stddev','median','max','min','range')
+
+bydevice<-ddply(canabalt,"device",function(df)c(length(df$score),round(mean(df$score)),round(sd(df$score)),median(df$score),max(df$score),min(df$score),max(df$score)-min(df$score)))
+names(bydevice) <- c('device','count','mean','stddev','median','max','min','range')
 
 p <- ggplot(canabalt,aes(factor(death),score))
 p + geom_boxplot()+coord_flip() + facet_wrap(~ device, ncol=1)
