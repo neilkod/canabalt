7fccbf3059aec48b39faaa6cbca3620a43f3f07f
neilkod <nkodner@gmail.com>
2011-02-07 17:21:24 -0500

    README

--- scores_by_death.r
+++ scores_by_death.r
@@ -1,7 +1,12 @@
 library('plyr')
 library('ggplot2')
-dat <- read.csv('data/canabaltdetailedscores.csv',stringsAsFactors=FALSE)
-names(dat)<-c('score','death','device')
-grpd<-ddply(dat,"death",function(df)c(length(df$score),round(mean(df$score)),round(sd(df$score)),median(df$score),max(df$score),min(df$score),max(df$score)-min(df$score)))
+canabalt <- read.csv('/Users/nkodner/Dropbox/development/R/canabalt/data/canabaltdetailedscores.csv',stringsAsFactors=FALSE)
+names(canabalt)<-c('score','death','device')
+
+# group on type of death and compute the count, mean, stddev, median, range.
+# we'll use this later
+grpd<-ddply(canabalt,"death",function(df)c(length(df$score),round(mean(df$score)),round(sd(df$score)),median(df$score),max(df$score),min(df$score),max(df$score)-min(df$score)))
 names(grpd) <- c('death','count','mean','stddev','median','max','min','range')
-qplot(grpd$mean,grpd$death)
+
+p <- ggplot(canabalt,aes(factor(death),score))
+p + geom_boxplot()+coord_flip() + facet_wrap(~ device, ncol=1)
